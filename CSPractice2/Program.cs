b0f2f3d41c2e8040cef6dadc3e796fc7109869a5
HanSol Kim <yg91238405@gmail.com>
2024-07-03 08:53:42 +0900

    #6 23-7 섀도잉

--- CSPractice2/Program.cs
+++ CSPractice2/Program.cs
@@ -8,8 +8,14 @@ namespace CSPractice2
 {
     internal class Program
     {
+        int number = 10;
         static void Main(string[] args)
         {
+            // #6 23-7 섀도잉
+            int number = 20;
+            Console.WriteLine(number); // 20
+
+
         }
     }
 }
